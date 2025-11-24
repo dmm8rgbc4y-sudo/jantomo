05a8f05bff09a03a632e18d9d687b5838072e0cc
dmm8rgbc4y-sudo <dmm8rgbc4y-sudo@users.noreply.github.com>
2025-11-24 12:47:21 +0900

    fix: restore missing Alembic revision (7763b5039fde)

---
+++ migrations/versions/7763b5039fde_dummy_restore.py
@@ -0,0 +1,12 @@
+"""restore missing revision"""
+
+revision = '7763b5039fde'
+down_revision = '16fe54434dcc'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
